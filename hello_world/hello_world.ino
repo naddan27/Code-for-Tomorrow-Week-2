ddb16215d3bdc7a46dbd94596ed13ff6beea5012
naddan27 <donghan_kim@brown.edu>
2018-07-16 13:16:48 -0400

    test

--- hello_world/hello_world.ino
+++ hello_world/hello_world.ino
@@ -1,15 +1,15 @@
 int gChromeBook = 100;
-void setup(){
- Serial.begin(9600);
- Serial.println("test");
- int localVariable = 1;
- Serial.println(gChromeBook);
- Serial.println(localVariable);
+void setup() {
+  Serial.begin(9600);
+  Serial.println("test");
+  int localVariable = 1;
+  Serial.println(gChromeBook);
+  Serial.println(localVariable);
+  Serial.println(localVariableNOTICE);
 }
 
-void loop(){
-  int localVariable1 = 2;
+void loop() {
+  int localVariableNOTICE = 2;
   Serial.println(gChromeBook);
-  Serial.println(localVariable1);
-  delay(100000);
+  Serial.println(localVariableNOTICE);
 }
