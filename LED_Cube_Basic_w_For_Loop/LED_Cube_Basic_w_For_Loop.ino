3f4eacde5d9a1dc5001a95ef9506a9ef10c95fee
naddan27 <donghan_kim@brown.edu>
2018-07-19 11:45:25 -0400

    yah

--- LED_Cube_Basic_w_For_Loop/LED_Cube_Basic_w_For_Loop.ino
+++ LED_Cube_Basic_w_For_Loop/LED_Cube_Basic_w_For_Loop.ino
@@ -1,19 +1,16 @@
 int columns[] = {A4, 11, 12, 13, A5, 8, 9, 10, 3, 6, 5, 7, 2, 1, 0, 4};
 int layers[] = {A0, A1, A2, A3};
+int activatedColumn = 0;
+int activatedLayer = 0;
+
 void setup() {
   setDefaultSettings();
-
-  digitalWrite(columns[3], HIGH);
-  digitalWrite(layers[0], LOW);
-
-  digitalWrite(columns[3], LOW);
-  digitalWrite(layers[0], HIGH);
-  digitalWrite(columns[12], HIGH);
-  digitalWrite(layers[3], LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  turnLEDOn(3, 2);
+
   
 }
 
@@ -28,5 +25,19 @@ void setDefaultSettings(){
     pinMode(layers[i], OUTPUT);
     digitalWrite(layers[i], HIGH);
   }
+}
+
+void turnLEDOn(int column, int layer){
+  //code to turn the old led off
+  digitalWrite(columns[activatedColumn], LOW);
+  digitalWrite(layers[activatedLayer], HIGH);
+
+  //code to redefine what is the activated column and layer
+  activatedColumn = column;
+  activatedLayer = layer;
+
+  //turn what we want turned on, on
+  digitalWrite(columns[activatedColumn], HIGH);
+  digitalWrite(layers[activatedLayer], LOW);
 }
 
