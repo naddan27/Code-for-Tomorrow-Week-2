eb89aa7cd23978603ea40da4ffed14d99b022a25
naddan27 <donghan_kim@brown.edu>
2018-07-19 10:22:28 -0400

    yah

--- LED_Cube_Basic_w_For_Loop/LED_Cube_Basic_w_For_Loop.ino
+++ LED_Cube_Basic_w_For_Loop/LED_Cube_Basic_w_For_Loop.ino
@@ -1,9 +1,20 @@
+int columns[] = {A4, 11, 12, 13, A5, 8, 9, 10, 3, 6, 5, 7, 2, 1, 0, 4};
+int layers[] = {A0, A1, A2, A3};
 void setup() {
-  // put your setup code here, to run once:
-
+  //Declaring all of the columns as OUTPUT pins
+  for (int i = 0; i < 16; i++){
+    pinMode(columns[i], OUTPUT);
+    digitalWrite(columns[i], LOW);
+  }
+  //Declaring all of the layers as OUTPUT pins
+  for (int i = 0; i <4 ; i++){
+    pinMode(layers[i], OUTPUT);
+    digitalWrite(layers[i], HIGH);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
+
+
